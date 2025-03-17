2af7dc6cbf411042796e48060f4010b8736629e1
cnlee1702 <daisies-dots.5t@icloud.com>
2025-03-17 14:39:36 -0400

    Update docker build_script.sh

--- docker/build_script.sh
+++ docker/build_script.sh
@@ -1,34 +1,47 @@
 #!/bin/bash
 
+log() {
+    local level="$1"
+    shift
+    local timestamp
+    timestamp="$(date '+%Y-%m-%dT%H:%M:%S')"
+    printf "[%s] [%s] %s\n" "$timestamp" "$level" "$*"
+}
+
 # Set image name
-IMAGE_NAME="ghcr.io/gmu-rtx-ground/gmu-rtx"
+DOCKER_IMAGE="${DOCKER_IMAGE:-ghcr.io/gmu-rtx-ground/gmu-rtx}"
 
 # Build Architectures
 ARCHS=(amd64 arm64)
 
 # Build images
 for ARCH in "${ARCHS[@]}"; do
-    echo "==> Building and pushing ${ARCH} image..."
+    log INFO "Building and pushing ${ARCH} image..."
     docker buildx build \
         --platform linux/${ARCH} \
         -f docker/Dockerfile.${ARCH} \
-        -t ${IMAGE_NAME}:${ARCH} \
+        -t ${DOCKER_IMAGE}:${ARCH} \
         --ssh default \
         --push \
         .
 done
 
 DIGEST_AMD64=$(docker inspect --format='{{index .RepoDigests 0}}' "${DOCKER_IMAGE}:amd64")
-echo "Digest for linux/amd64: ${DIGEST_AMD64}"
+log INFO "Digest for linux/amd64: ${DIGEST_AMD64}"
 DIGEST_ARM64=$(docker inspect --format='{{index .RepoDigests 0}}' "${DOCKER_IMAGE}:arm64")
-echo "Digest for linux/arm64: ${DIGEST_ARM64}"
+log INFO "Digest for linux/arm64: ${DIGEST_ARM64}"
+DIGEST_LATEST=$(docker inspect --format='{{index .RepoDigests 0}}' "${DOCKER_IMAGE}:latest")
+log INFO "Digest for multi-platform image: ${DIGEST_LATEST}"
+
+docker image rm "${DIGEST_LATEST}"
 
 # Create and publish new multi-platform image
-echo "==> pushing ${IMAGE_NAME}:latest image..."
-docker buildx imagetools create -t "${IMAGE_NAME}:latest" \
-    ${DIGEST_AMD64} \
-    ${DIGEST_ARM64}
+log INFO "Building ${DOCKER_IMAGE}:latest image..."
+docker buildx imagetools create -t "${DOCKER_IMAGE}:latest" \
+        "${DIGEST_AMD64}" \
+        "${DIGEST_ARM64}"
 
-docker push "${IMAGE_NAME}:latest"
+echo "[Info] : Pushing ${DOCKER_IMAGE}:latest"
+docker push "${DOCKER_IMAGE}:latest"
 
 echo "Complete!"
