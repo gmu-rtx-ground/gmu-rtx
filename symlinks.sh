12d6739619303dade007e01138e92c5dc75de472
Teyema <lpr1m3@icloud.com>
2025-04-07 04:08:33 +0530

    Updates to symlinks and ekf and losi config/launch files

---
+++ symlinks.sh
@@ -0,0 +1,6 @@
+ln -s "/home/${USER}/gmu-rtx/ROS/gps_robot_localization" "${CATKIN_WS}/src/gps_robot_localization" && \
+ln -s "/home/${USER}/gmu-rtx/ROS/losi_launch" "${CATKIN_WS}/src/losi_launch" && \
+ln -s "/home/${USER}/gmu-rtx/ROS/realsense-ros" "${CATKIN_WS}/src/realsense-ros" && \
+ln -s "/home/${USER}/gmu-rtx/ROS/ublox-F9P" "${CATKIN_WS}/src/ublox-F9P" && \
+ln -s "/home/${USER}/gmu-rtx/ROS/vision_opencv" "${CATKIN_WS}/src/vision_opencv" && \
+ln -s "/home/${USER}/gmu-rtx/ROS/witmotion_IMU_ros" "${CATKIN_WS}/src/witmotion_IMU_ros"
