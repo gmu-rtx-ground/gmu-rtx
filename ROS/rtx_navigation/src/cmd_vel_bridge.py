17054efc3f866f91359b35e480680ddd1c665ccd
Logan <lpr1m3@icloud.com>
2025-04-11 17:32:06 -0400

    Add cmd_vel bridge file

---
+++ ROS/rtx_navigation/src/cmd_vel_bridge.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+import rospy
+from geometry_msgs.msg import Twist
+from std_msgs.msg import Float32MultiArray
+
+def cmd_vel_callback(msg):
+    out = Float32MultiArray()
+    # Example conversion: linear.x → throttle, angular.z → steering
+    out.data = [msg.angular.z, msg.linear.x, 0.0]
+    pub.publish(out)
+
+rospy.init_node('cmd_vel_bridge')
+pub = rospy.Publisher('/cmd_vel1', Float32MultiArray, queue_size=10)
+rospy.Subscriber('/cmd_vel', Twist, cmd_vel_callback)
+rospy.spin()
