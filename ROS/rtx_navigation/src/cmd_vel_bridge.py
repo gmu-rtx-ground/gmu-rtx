d1906915aec5fa836e51649a763782ff98ffa98e
Teyema <lpr1m3@icloud.com>
2025-04-20 20:07:52 +0530

    Refactored ROS

--- ROS/rtx_navigation/src/cmd_vel_bridge.py
+++ ROS/rtx_navigation/src/cmd_vel_bridge.py
@@ -2,11 +2,14 @@
 import rospy
 from geometry_msgs.msg import Twist
 from std_msgs.msg import Float32MultiArray
+import numpy as np
 
 def cmd_vel_callback(msg):
-    out = Float32MultiArray()
+    out = Float32MultiArray(data=[0.0, 0.0, 0.0])
     # Example conversion: linear.x -> throttle, angular.z ->steering
-    out.data = [msg.angular.z, msg.linear.x, 0.0]
+    out.data[0] = -np.clip(msg.angular.z*6.25, -1.0, 1.0)  # Convert to steering
+    out.data[1] = np.clip(msg.linear.x*2, -1.0, 1.0)  # Convert to throttle
+    
     pub.publish(out)
 
 rospy.init_node('cmd_vel_bridge')
